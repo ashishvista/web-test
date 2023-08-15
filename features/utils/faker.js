0c0e5f9c87b8b6d055a96a7d76c7fc1f6b4f53c1
ashish <ashish.kumar@rapimoney.com>
2023-08-15 20:36:15 +0530

    all

--- features/utils/faker.js
+++ features/utils/faker.js
@@ -2,7 +2,7 @@ import { faker } from '@faker-js/faker';
 
 export const randomData = {
     name: faker.internet.userName(),
-    length: faker.number.int(19),
-    width: faker.number.int(19),
-    height: faker.number.int(19),
+    length: faker.number.int({ min: 1, max: 19 }),
+    width: faker.number.int({ min: 1, max: 19 }),
+    height: faker.number.int({ min: 1, max: 19 }),
 };
